27e0b305fc8d18b1ac5591a9413cc48b5205555f
Joshua Kolden <joshua@avalanche.io>
2026-03-26 17:04:37 -0500

    Update c4 formula to v1.0.12

--- Formula/c4.rb
+++ Formula/c4.rb
@@ -1,26 +1,26 @@
 class C4 < Formula
   desc "C4 Universal Content Identification — CLI tools (SMPTE ST 2114)"
   homepage "https://cccc.io"
-  version "1.0.10"
+  version "1.0.12"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Avalanche-io/c4toolkit/releases/download/v1.0.10/c4_darwin_arm64.tar.gz"
-      sha256 "98de00e25da08b6d5fa385b9a6a4c61c490acb6b06e7494ed56a2b1a060ca8f6"
+      url "https://github.com/Avalanche-io/c4toolkit/releases/download/v1.0.12/c4_darwin_arm64.tar.gz"
+      sha256 "e20c682b5587bf51b923d40f09f0380496a65658aaaa200852921e5b6a966744"
     else
-      url "https://github.com/Avalanche-io/c4toolkit/releases/download/v1.0.10/c4_darwin_amd64.tar.gz"
-      sha256 "1ef66dda01dd5674d7b3301736651a56fd9e4c6ffe4522423fead93a6d604542"
+      url "https://github.com/Avalanche-io/c4toolkit/releases/download/v1.0.12/c4_darwin_amd64.tar.gz"
+      sha256 "054e6f77f61513479012675adbd5f04da6773faa5ab60dfd02794fb48e800940"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/Avalanche-io/c4toolkit/releases/download/v1.0.10/c4_linux_arm64.tar.gz"
-      sha256 "84b4aa4f3015fc96aa68425ccbde2ce868348e68a36f9a05938029ccceac1215"
+      url "https://github.com/Avalanche-io/c4toolkit/releases/download/v1.0.12/c4_linux_arm64.tar.gz"
+      sha256 "38251d454e4710a16b296186fa5f2641eec37f79be5d2b10947f99ec03875612"
     else
-      url "https://github.com/Avalanche-io/c4toolkit/releases/download/v1.0.10/c4_linux_amd64.tar.gz"
-      sha256 "ecb5860207706b63b7288f6ac456c2ef3bec3b0a4614cba66dfb06c8926ab71f"
+      url "https://github.com/Avalanche-io/c4toolkit/releases/download/v1.0.12/c4_linux_amd64.tar.gz"
+      sha256 "14064695aa2773a1b0e63dcf253f8cd3127acfc33d131557559724698894528d"
     end
   end
 
