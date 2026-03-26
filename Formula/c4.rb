2f1c26bdd2f151deeaf76475e89065f56f4cdb0f
Joshua Kolden <joshua@avalanche.io>
2026-03-26 10:39:16 -0500

    Update c4 suite to v1.0.10

--- Formula/c4.rb
+++ Formula/c4.rb
@@ -1,26 +1,26 @@
 class C4 < Formula
   desc "C4 Universal Content Identification — CLI tools (SMPTE ST 2114)"
   homepage "https://cccc.io"
-  version "1.0.6"
+  version "1.0.10"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Avalanche-io/c4-releases/releases/download/v1.0.6/c4-suite_v1.0.6_darwin_arm64.tar.gz"
-      sha256 "b4c1c00d62f6043abda81576f1854231ee3b5edcf970eed4f590e878bae47682"
+      url "https://github.com/Avalanche-io/c4toolkit/releases/download/v1.0.10/c4_darwin_arm64.tar.gz"
+      sha256 "98de00e25da08b6d5fa385b9a6a4c61c490acb6b06e7494ed56a2b1a060ca8f6"
     else
-      url "https://github.com/Avalanche-io/c4-releases/releases/download/v1.0.6/c4-suite_v1.0.6_darwin_amd64.tar.gz"
-      sha256 "2fd31c710e5311da4560f30555cc3ea811aeb352bbef148cb7945feccf84af19"
+      url "https://github.com/Avalanche-io/c4toolkit/releases/download/v1.0.10/c4_darwin_amd64.tar.gz"
+      sha256 "1ef66dda01dd5674d7b3301736651a56fd9e4c6ffe4522423fead93a6d604542"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/Avalanche-io/c4-releases/releases/download/v1.0.6/c4-suite_v1.0.6_linux_arm64.tar.gz"
-      sha256 "8986379a72b4e2132f63d30766629bf6cef6967a81ffd48a9d93db1f29e6648f"
+      url "https://github.com/Avalanche-io/c4toolkit/releases/download/v1.0.10/c4_linux_arm64.tar.gz"
+      sha256 "84b4aa4f3015fc96aa68425ccbde2ce868348e68a36f9a05938029ccceac1215"
     else
-      url "https://github.com/Avalanche-io/c4-releases/releases/download/v1.0.6/c4-suite_v1.0.6_linux_amd64.tar.gz"
-      sha256 "670c30a80bcc89290768380c8261f0f61932a2ef579c3743169d46ccea6fba03"
+      url "https://github.com/Avalanche-io/c4toolkit/releases/download/v1.0.10/c4_linux_amd64.tar.gz"
+      sha256 "ecb5860207706b63b7288f6ac456c2ef3bec3b0a4614cba66dfb06c8926ab71f"
     end
   end
 
