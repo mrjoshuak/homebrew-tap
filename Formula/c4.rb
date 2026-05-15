5331da8341313a74f169c0e9a3c29457993df22f
Joshua Kolden <joshua@avalanche.io>
2026-05-15 10:04:37 -0500

    Update c4 formula to v1.0.13

--- Formula/c4.rb
+++ Formula/c4.rb
@@ -1,26 +1,26 @@
 class C4 < Formula
   desc "C4 Universal Content Identification — CLI tools (SMPTE ST 2114)"
   homepage "https://cccc.io"
-  version "1.0.12"
+  version "1.0.13"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Avalanche-io/c4toolkit/releases/download/v1.0.12/c4_darwin_arm64.tar.gz"
-      sha256 "e20c682b5587bf51b923d40f09f0380496a65658aaaa200852921e5b6a966744"
+      url "https://github.com/Avalanche-io/c4toolkit/releases/download/v1.0.13/c4-suite_v1.0.13_darwin_arm64.tar.gz"
+      sha256 "71d65a6d7849ee3404de6ff70e6b58b53370178b3e7bc8398c6ec4c84f7d17d4"
     else
-      url "https://github.com/Avalanche-io/c4toolkit/releases/download/v1.0.12/c4_darwin_amd64.tar.gz"
-      sha256 "054e6f77f61513479012675adbd5f04da6773faa5ab60dfd02794fb48e800940"
+      url "https://github.com/Avalanche-io/c4toolkit/releases/download/v1.0.13/c4-suite_v1.0.13_darwin_amd64.tar.gz"
+      sha256 "92ee83675ca3cd35c850ed1dd506da2e1850f19c75ff8bec886bbb3419ee7f2c"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/Avalanche-io/c4toolkit/releases/download/v1.0.12/c4_linux_arm64.tar.gz"
-      sha256 "38251d454e4710a16b296186fa5f2641eec37f79be5d2b10947f99ec03875612"
+      url "https://github.com/Avalanche-io/c4toolkit/releases/download/v1.0.13/c4-suite_v1.0.13_linux_arm64.tar.gz"
+      sha256 "44fd964012985c2b828f384baf58086dfedcc805a9b15ccbf14c6b1d82e1c0e9"
     else
-      url "https://github.com/Avalanche-io/c4toolkit/releases/download/v1.0.12/c4_linux_amd64.tar.gz"
-      sha256 "14064695aa2773a1b0e63dcf253f8cd3127acfc33d131557559724698894528d"
+      url "https://github.com/Avalanche-io/c4toolkit/releases/download/v1.0.13/c4-suite_v1.0.13_linux_amd64.tar.gz"
+      sha256 "23399b450afbc62bd6348affef1a04a673e460a0e3b470a1a7f6cbe3db5ddfce"
     end
   end
 
@@ -31,7 +31,6 @@ class C4 < Formula
   end
 
   test do
-    assert_match "c4 1", shell_output("#{bin}/c4 version")
-    assert_match "c4sh 1", shell_output("#{bin}/c4sh version")
+    assert_match "c4 ", shell_output("#{bin}/c4 version")
   end
 end
